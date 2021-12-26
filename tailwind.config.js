075823c497571911558120d2c05e974d812fd9b1
Carlos Castillo <47466248+cecp01@users.noreply.github.com>
2021-12-25 22:08:46 -0300

    feat: think I finished fish version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
 	content: ["./index.html"],
 	theme: {
 		extend: {
+			colors: {
+				langs: {
+					lua: "#000080",
+					rust: "#dea584",
+					shell: "#89e051"
+				}
+			},
 			fontFamily: {
 				lora: "'Lora', serif"
 			}
